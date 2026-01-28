e20cd89b67c248932ac314a917ca5c5bf727c428
RavinduPe <raviduy60@gmail.com>
2026-01-28 11:53:26 +0530

    Initial commit

---
+++ src/main/java/com/example/LibraryManagementSystem/entity/Book.java
@@ -0,0 +1,12 @@
+package com.example.LibraryManagementSystem.entity;
+
+import javax.persistence.Id;
+
+public class Book {
+    @Id
+    private long id;
+    private String title;
+    private String author;
+    private
+
+}
